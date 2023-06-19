201bda72099f342a0ef120e78959cbce12190021
cristianbdl <137109939+cristianbdl@users.noreply.github.com>
2023-06-19 15:19:38 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
+
+# Configurar los orígenes permitidos (* permite todos los orígenes)
+origins = ["*"]
+
+# Agregar el middleware CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Variable para almacenar los datos en forma de diccionario
 usuarios = {}
